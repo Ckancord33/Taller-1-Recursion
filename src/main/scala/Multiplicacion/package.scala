b382b23de218571c2de05b43f6afb0bd925fd1de
SamuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2025-03-03 18:37:28 -0500

    cambios

--- src/main/scala/Multiplicacion/package.scala
+++ src/main/scala/Multiplicacion/package.scala
@@ -10,7 +10,7 @@ package object Multiplicacion{
     def iterativeAlgorithm(a: Int, b: Int, result: Int): Int = {
       def isOdd: Int = iterativeAlgorithm(a / 2, b + b, result)
       def isOven: Int = iterativeAlgorithm(a / 2, b + b, result + b)
-      if (a != 0) if (a%2==0) isOdd else isOven else result
+      if (a == 0) result else if (a%2==0) isOdd else isOven
     }
     iterativeAlgorithm(a, b, 0)
   }
@@ -19,13 +19,11 @@ package object Multiplicacion{
 
     def numberDigits(c: Int, nDigits: Int): Int = if(c == 0)  nDigits  else numberDigits(c / 10, nDigits + 1);
 
-    def abs(x: Int) = if(x >= 0) a else -x
-
-    if (abs(a) < 10 && abs(b) < 10) return a * b
-
+    if (a < 10 && b < 10) a * b
+    else{
     val numberDigitA: Int = numberDigits(a, 0) / 2
     val numberDigitB: Int = numberDigits(b, 0) / 2
-    val nFinal: Int = if (numberDigitA >= numberDigitB) numberDigitA else numberDigitB //PREGUNTAR AL PROFESOR...
+    val nFinal: Int = if (numberDigitA >= numberDigitB) numberDigitA else numberDigitB
 
     def defTopPart(d: Int, nDigits: Int): Int = d / Math.pow(10, nDigits).toInt
     def defBottomPart(e: Int, numberDigit: Int): Int = e % math.pow(10, numberDigit).toInt
@@ -39,25 +37,21 @@ package object Multiplicacion{
     def secondStep: Int = Math.pow(10, nFinal).toInt * ( splitMultiply(bottomPartA,topPartB) + splitMultiply(bottomPartB,topPartA) )
     def thirdStep: Int = splitMultiply(bottomPartA,bottomPartB)
 
-    def algorithmMethod(): Int = firstStep + secondStep + thirdStep
-
-    algorithmMethod();
+    firstStep + secondStep + thirdStep
+    }
   }
 
 
   def fastMultiply(a: Int, b: Int): Int = {
     def numberDigits(c: Int, nDigits: Int): Int = if (c == 0) nDigits else numberDigits(c / 10, nDigits + 1)
-
-    def abs(x: Int) = if(x >= 0) a else -x
-
-    if (abs(a) < 10 && abs(b) < 10) return a * b
-
+    
+    if (a < 10 && b < 10) a * b
+    else{
     val numberDigitA: Int = numberDigits(a, 0) / 2
     val numberDigitB: Int = numberDigits(b, 0) / 2
     val nFinal: Int = if (numberDigitA >= numberDigitB) numberDigitA else numberDigitB
 
     def defTopPart(d: Int, nDigits: Int): Int = d / Math.pow(10, nDigits).toInt
-
     def defBottomPart(e: Int, numberDigit: Int): Int = e % math.pow(10, numberDigit).toInt
 
     val topPartA = defTopPart(a, nFinal)
@@ -69,14 +63,11 @@ package object Multiplicacion{
     val bottomRecycled = fastMultiply(bottomPartA, bottomPartB)
 
     def firstStep: Int = Math.pow(10, nFinal * 2).toInt * topRecycled
-
     def secondStep: Int = Math.pow(10, nFinal).toInt * (topRecycled + bottomRecycled - fastMultiply((topPartA - bottomPartA),(topPartB - bottomPartB)))
-
     def thirdStep: Int = bottomRecycled
 
-    def algorithmMethod(): Int = firstStep + secondStep + thirdStep
-
-    algorithmMethod()
-
+    firstStep + secondStep + thirdStep
+      
+    }
   }
 }
--- target/scala-3.6.3/test-classes/pruebas.sc
+++ target/scala-3.6.3/test-classes/pruebas.sc
@@ -1,19 +1,25 @@
 import Multiplicacion._
 
-PeasantAlgorithm(5,2)
-PeasantAlgorithm(7,3)
-PeasantAlgorithm(8,4)
-PeasantAlgorithm(2145,3233)
+PeasantAlgorithm(0,34)
+PeasantAlgorithm(0,12)
+PeasantAlgorithm(50,1)
+PeasantAlgorithm(1,1234)
 
-PeasantAlgorithmIt(5,2)
-PeasantAlgorithmIt(7,3)
-PeasantAlgorithmIt(8,4)
-PeasantAlgorithmIt(2145,3233)
+PeasantAlgorithmIt(0,34)
+PeasantAlgorithmIt(12,0)
+PeasantAlgorithmIt(50,1)
+PeasantAlgorithmIt(1,1234)
+PeasantAlgorithmIt(13,4)
+PeasantAlgorithmIt(12,15)
+PeasantAlgorithmIt(12345,43)
+PeasantAlgorithmIt(1234566,988)
+
+
 
 splitMultiply(4,5)
 splitMultiply(2145,3233)
 splitMultiply(14,32)
 
-fastMultiply(4,5)
-fastMultiply(2145,3233)
-fastMultiply(14,32)
+fastMultiply(4,20)
+fastMultiply(2145,123)
+fastMultiply(14454,32540)
